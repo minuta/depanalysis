bf110965047f242de9d570a9355c8b2bd34082cf
AF <gammamustang@gmail.com>
2020-11-23 18:36:58 +0100

    update test_tool.py

--- test_tool.py
+++ test_tool.py
@@ -1,11 +1,13 @@
 import pytest
 import tool
 
+JDEPS_VERSION = "11.0.8"
+VERSION_COMMAND = "--version"
 
 def test_jdeps_availability():
     assert tool.is_jdeps_available()
 
 
 def test_jdeps_with_params():
-    jdeps_version = tool.run_jdeps(["--version"])
-    assert jdeps_version == "11.0.8"
+    jdeps_version = tool.run_jdeps([VERSION_COMMAND])
+    assert jdeps_version == JDEPS_VERSION

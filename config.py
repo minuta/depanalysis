d2bb462e7c728086986706da47ade30c175030a6
AF <gammamustang@gmail.com>
2020-11-24 20:41:19 +0100

    update class Config : add an exception handling

--- config.py
+++ config.py
@@ -2,17 +2,27 @@ import json
 
 class Config:
 
+    CONFIG_FILE_NAME = 'config.json'
+
     def __init__(self):
         self.settings = self.read_path_from_config()
 
+
     def read_path_from_config(self):
-        with open('config.json') as f:
-            settings = json.load(f)
-        return settings
+        config_data = None
+        try:
+            with open(self.CONFIG_FILE_NAME) as f:
+                config_data = json.load(f)
+        except Exception as ex:
+            print('ERROR: could not read the config file: {0}'.format(ex))
+        return config_data
 
     def get_path(self):
         return self.settings["JDEPS_PATH"]
 
+    def get_all_config_data(self):
+        return self.settings
+
 if __name__ == "__main__":
     cfg = Config()
-    print(cfg.read_path_from_config())
+    print(cfg.get_all_config_data())
